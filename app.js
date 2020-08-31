f5a0d3937ec4b67960b20e026adfeef760f81a69
scbonner <codingbootcamp918@gmail.com>
2020-08-30 20:00:02 -0400

    adding first workspace for jsdom

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const myHeading = document.getElementById('myHeading')   //select the element
+
+myHeading.addEventListener('click', () => {  //call and passing a function
+  myHeading.style.color = 'red';
+});
