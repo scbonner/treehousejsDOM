ecb9c5d99e7243250bcc80d088c0a0d2e45c212b
scbonner <codingbootcamp918@gmail.com>
2020-08-30 22:21:14 -0400

    added a reset button to app

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 
-const myHeading = document.getElementById('myHeading')   //select the element
+const myHeading = document.getElementById('myHeading');   //select the element
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+const resetButton = document.getElementById('resetButton');
+const resetInput = document.getElementById('resetInput');
 
-myHeading.addEventListener('click', () => {  //call and passing a function
-  myHeading.style.color = 'red';
+myButton.addEventListener('click', () => {  //call and passing a function
+  myHeading.style.color = myTextInput.value;
+});
+resetButton.addEventListener('click', () => { 
+  myHeading.style.color = resetInput.value;
 });
