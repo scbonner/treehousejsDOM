6bf863e89dc75e9c4bbeee2decb62e4efc79e839
scbonner <codingbootcamp918@gmail.com>
2020-08-31 19:34:18 -0400

    adding changes to file

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 const resetButton = document.getElementById('resetButton');
 const resetInput = document.getElementById('resetInput');
+const jumboButton = document.getElementById('jumboButton')
+const jumboInput = document.getElementById('jumboInput')
 
 myButton.addEventListener('click', () => {  //call and passing a function
   myHeading.style.color = myTextInput.value;
 });
 resetButton.addEventListener('click', () => { 
   myHeading.style.color = resetInput.value;
+});
+jumboButton.addEventListener('click', () => {
+  myHeading.style.color = 
 });
