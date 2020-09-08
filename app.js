d243bdc6f50861b56ce3a90f9b18ce16b8143239
scbonner <codingbootcamp918@gmail.com>
2020-09-07 21:03:45 -0400

    updating files

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
 const myHeading = document.getElementById('myHeading');   //select the element
 const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
-const resetButton = document.getElementById('resetButton');
-const resetInput = document.getElementById('resetInput');
+// const myTextInput = document.getElementById('myTextInput');
+// const resetButton = document.getElementById('resetButton');
+// const resetInput = document.getElementById('resetInput');
 const jumboButton = document.getElementById('jumboButton')
 const jumboInput = document.getElementById('jumboInput')
 
 myButton.addEventListener('click', () => {  //call and passing a function
-  myHeading.style.color = myTextInput.value;
-});
-resetButton.addEventListener('click', () => { 
-  myHeading.style.color = resetInput.value;
-});
-jumboButton.addEventListener('click', () => {
-  myHeading.style.color = 
-});
+myHeading.style.color = 'red'});  
+  // myTextInput.value;
+// resetButton.addEventListener('click', () => { 
+//   myHeading.style.color = resetInput.value;
+// });
+// jumboButton.addEventListener('click', () => {
+//   myHeading.style.color = 
+// });
