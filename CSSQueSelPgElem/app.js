95840795fb27d377bc4f0dac8bfa7e8e21624d92
scbonner <codingbootcamp918@gmail.com>
2020-09-30 06:46:02 -0400

    adding file

---
+++ CSSQueSelPgElem/app.js
@@ -0,0 +1,26 @@
+const myList = document.getElementsByClassName('li');
+
+for (let i = 0; i < myList.length; i += 1) {
+    myList[i].style.color = 'purple';
+
+}
+
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+    errorNotPurple[i].style.color = 'red';
+    
+}
+
+const evens = document.querySelectorAll('li:nth-child(even)');
+
+for (let i = 0; i < evens.length; i += 1) {
+    evens[i].style.backgroundColor = 'lightgray';
+    
+}
+const odds = document.querySelectorAll('li:nth-child(even)');
+
+for (let i = 0; i < evens.length; i += 1) {
+    odds[i].style.backgroundColor = 'teal';
+    
+}
